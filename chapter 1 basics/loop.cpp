d2c1292256c3ad94f626999748dc4e59b3719f8a
chaudhary-rajkumar <prorajkumar555@gmail.com>
2024-08-05 17:11:08 +0545

    chapter 1 basics completed

---
+++ chapter 1 basics/loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*For loop in C++*/
+
+    for (int  i = 0; i < 5; i++)
+    {
+        cout<<"The value of i is "<<i<<endl;
+    }
+    
+
+
+    return 0;
+}
