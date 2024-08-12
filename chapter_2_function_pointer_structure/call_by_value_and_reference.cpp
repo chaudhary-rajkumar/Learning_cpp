a2d8cbddd83f1a816c0ada192e89ce9101de8b5e
chaudhary-rajkumar <prorajkumar555@gmail.com>
2024-08-12 15:52:43 +0545

    chapter 2 finished

--- chapter_2_function_pointer_structure/call_by_value_and_reference.cpp
+++ chapter_2_function_pointer_structure/call_by_value_and_reference.cpp
@@ -1,26 +1,44 @@
-#include<iostream>
-using namespace std;
+// this will not swap the values of function
+// ---> function can not change actual value
+// call by value 
 
-// This will not swap a and b 
-// int swap(int a, int b){
+// #include<iostream>
+// using namespace std;
+
+// void swap(int a,int b){
 //     int temp=a;
 //     a=b;
 //     b=temp;
 // }
+// int main(){
+//     int x=4;
+//     int y=5;
+    // cout<<"The value of x is "<<x<<"\nThe value of y is "<<y<<endl;
+    // // swap(x,y);
+    // swap(x,y);
+    // cout<<"The value of x is "<<x<<"\nThe value of y is "<<y<<endl;
+
+//     return 0;
+// }
 
 
+
+
+
+// But by changing address we can change the value 
+// Call by reference using pointers
+#include<iostream>
+using namespace std;
 void swapPointer(int *a, int *b){
     int temp=*a;
     *a=*b;
     *b=temp;
 }
 int main(){
-    int x=4;
-    int y=5;
-    cout<<"The value of x is "<<x<<"\nthe value of y is "<<y<<endl;
-    
-    cout<<"The value of x is "<<x<<"\nthe value of y is "<<y<<endl;
-    
-
+    int x=4,  y=5;
+    cout<<"The value of x is "<<x<<"\nThe value of y is "<<y<<endl;
+    // swap(x,y);
+    swap(x,y);
+    cout<<"The value of x is "<<x<<"\nThe value of y is "<<y<<endl;
     return 0;
 }
